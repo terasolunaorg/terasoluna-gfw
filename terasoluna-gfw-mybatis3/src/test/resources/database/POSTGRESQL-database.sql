d9fb197b483b55a462a158814fd73460daa52c33
Kazuki Shimizu <shimizukza@pm.nttdata.co.jp>
2014-03-03 16:33:38 +0900

    add sub project for mybatis3. #2

--- terasoluna-gfw-mybatis3/src/test/resources/database/POSTGRESQL-database.sql
+++
@@ -1,7 +0,0 @@
-DROP DATABASE IF EXISTS terasoluna_gfw_mybatis3;
-
-CREATE DATABASE terasoluna_gfw_mybatis3
-  WITH OWNER = postgres
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       CONNECTION LIMIT = -1;
