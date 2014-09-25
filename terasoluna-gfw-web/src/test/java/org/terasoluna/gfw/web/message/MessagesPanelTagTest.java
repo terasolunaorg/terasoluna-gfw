231542c474a4ba38cf28d495f6a3d7ba6204c1bb
tatsutanik <tatsutanik@pm.nttdata.co.jp>
2014-09-25 11:43:04 +0900

    remove unneccesary implements from MessagePanelTagTest #138

--- terasoluna-gfw-web/src/test/java/org/terasoluna/gfw/web/message/MessagesPanelTagTest.java
+++ terasoluna-gfw-web/src/test/java/org/terasoluna/gfw/web/message/MessagesPanelTagTest.java
@@ -37,13 +37,10 @@ import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockPageContext;
 import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.annotation.ExpectedException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.tags.form.TagWriter;
 import org.terasoluna.gfw.common.message.ResultMessage;
 import org.terasoluna.gfw.common.message.ResultMessages;
-import org.terasoluna.gfw.web.message.MessagesPanelTag;
 
 /**
  * Test class for MessagesPanelTag.
