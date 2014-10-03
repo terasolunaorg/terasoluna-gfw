f27f4d7e294b31c76d5a752d4edbd8c4df4edc7e
btkatoutkm <katoutkm@pm.nttdata.co.jp>
2014-10-03 16:37:42 +0900

    Deleted unused import  #109

--- terasoluna-gfw-web/src/main/java/org/terasoluna/gfw/web/exception/ExceptionLoggingFilter.java
+++ terasoluna-gfw-web/src/main/java/org/terasoluna/gfw/web/exception/ExceptionLoggingFilter.java
@@ -21,11 +21,8 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.filter.GenericFilterBean;
-import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.terasoluna.gfw.common.exception.ExceptionLogger;
 
 /**
