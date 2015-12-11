b95b38ba8aa9b2e7541f76b00cd1a221075c3d9c
Toshiaki Maki <makingx@gmail.com>
2015-12-11 11:13:29 +0900

    Polish equals #367

--- terasoluna-gfw-string/src/main/java/org/terasoluna/gfw/common/fullhalf/FullHalfPair.java
+++ terasoluna-gfw-string/src/main/java/org/terasoluna/gfw/common/fullhalf/FullHalfPair.java
@@ -73,15 +73,18 @@ public final class FullHalfPair implements Serializable {
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
-        if (o == null || getClass() != o.getClass())
+        }
+        if (o == null || getClass() != o.getClass()) {
             return false;
+        }
 
         FullHalfPair that = (FullHalfPair) o;
 
-        if (!fullwidth.equals(that.fullwidth))
+        if (!fullwidth.equals(that.fullwidth)) {
             return false;
+        }
         return halfwidth.equals(that.halfwidth);
 
     }
