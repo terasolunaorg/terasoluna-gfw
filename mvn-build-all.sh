09d7acf04c66ee3101d09bc732d9907fe206ba59
Kazuki Shimizu <kazuki43zoo@gmail.com>
2016-01-05 01:19:09 +0900

    Add list file to manage build target #373

--- mvn-build-all.sh
+++ mvn-build-all.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-ARTIFACT_ID_PREFIX="terasoluna-gfw-"
-TARGETS="parent common jodatime web security-core security-web jpa mybatis3 mybatis2 recommended-dependencies recommended-web-dependencies"
+TARGETS=`cat mvn-build-all-targets.txt`
 DEFAULT_GOALS="clean install"
 
 commandArgs=${DEFAULT_GOALS}
@@ -13,8 +12,7 @@ echo "[INFO] Start a build."
 
 echo "[DEBUG] Command arguments : \"${commandArgs}\""
 
-for target in ${TARGETS}; do
-    artifactId=${ARTIFACT_ID_PREFIX}${target}
+for artifactId in ${TARGETS}; do
     if test -f ${artifactId}/pom.xml ; then
         mvn -U -f ${artifactId}/pom.xml ${commandArgs}
         buildResult=$?
