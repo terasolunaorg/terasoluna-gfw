f5e44c0a7750ca591dd79c73e98344da6d0d4977
btonokit <Keita.Ono.bp@nttdata.com>
2023-03-03 14:22:27 +0900

    added @SuppressWarnings for jodatime tests #1222

--- terasoluna-gfw-common-libraries/terasoluna-gfw-jodatime/src/test/java/org/terasoluna/gfw/common/date/jodatime/JdbcAdjustedJodaTimeDateFactoryTest.java
+++ terasoluna-gfw-common-libraries/terasoluna-gfw-jodatime/src/test/java/org/terasoluna/gfw/common/date/jodatime/JdbcAdjustedJodaTimeDateFactoryTest.java
@@ -35,6 +35,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+@SuppressWarnings("deprecation")
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:test-context.xml" })
 @Transactional
