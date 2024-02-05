d22d2ae65c3ada8a4e7a7886bca39c458efe7755
btkatoufm <Fumio.Kato.bp@nttdata.com>
2024-02-05 14:15:10 +0900

    update copyright. #1280

--- terasoluna-gfw-common-libraries/terasoluna-gfw-validator/src/test/java/org/terasoluna/gfw/common/validator/constraintvalidators/ConstraintValidatorsUtilsTest.java
+++ terasoluna-gfw-common-libraries/terasoluna-gfw-validator/src/test/java/org/terasoluna/gfw/common/validator/constraintvalidators/ConstraintValidatorsUtilsTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright(c) 2013 NTT DATA Corporation.
+ * Copyright(c) 2024 NTT DATA Group Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
